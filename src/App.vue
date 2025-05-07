fa117b4352d13d0ec7eca1a69edfba29cba8a457
droidbetfront01 <70860854+droidbetfront01@users.noreply.github.com>
2025-05-07 20:24:59 +0300

    Vue Start
    Init vue3 project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+   <main>
+     Hello world
+   </main>
+   <!-- <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+  <RouterView /> -->
+</template>
+
+<script setup>
+//import { RouterLink, RouterView } from 'vue-router'
+//import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+
+
+<style lang="scss" scoped>
+</style>
